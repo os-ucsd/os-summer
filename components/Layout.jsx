dd01b640886206a911420c308b020ab054bb3064
Glitch (asg017-mdx-next-test) <none>
2019-07-12 05:51:12 +0000

    🚘☺️ Checkpoint ./components/Layout.jsx:1856263/535 ./pages/index.mdx:1856263/3928

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,29 +9,15 @@ export default function Layout(props) {
   return (
     <div style={layoutStyle}>
       {props.children}
-      <style jsx>{`
+      <style global jsx>{`
         h1,
         a {
           font-family: 'Arial';
           color: red;
         }
-
-        ul {
-          padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
+        p {
+          font-family:  "Roboto Mono";
+    font-style: normal;
         }
       `}</style>
     </div>
